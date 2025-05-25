e8257e8425cdbacc9eab6612a0dbb6f2980982e3
Aman Jyoti <amanjyot20@gmail.com>
2025-05-25 16:52:08 +0530

    WaitMethods

--- src/test/java/Browser_and_Navigation/HandleBrowserWindow.java
+++ src/test/java/Browser_and_Navigation/HandleBrowserWindow.java
@@ -1,6 +1,9 @@
 package Browser_and_Navigation;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,10 +14,20 @@ public class HandleBrowserWindow {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//a[normalize-space()='OrangeHRM, Ins']")).click();
+		driver.findElement(By.xpath("//a[normalize-space()='OrangeHRM, Inc']")).click();
+		
+		Set<String> windowId = driver.getWindowHandles();
+		
+		//Approach 1
+		List <String> windowList= new ArrayList(windowId);
+		String parentId = windowList.get(0);
+		String childId = windowList.get(1);
+		driver.switchTo().window(childId);    //Switch to the child window
+		System.out.println(driver.getTitle());
+				
 		
 	}
 
---
+++ src/test/java/WaitMethods/ExpilicitWaitCommand.java
@@ -0,0 +1,28 @@
+package WaitMethods;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExpilicitWaitCommand 
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+        WebDriver driver = new ChromeDriver();
+        
+        WebDriverWait waitTime = new WebDriverWait(driver, Duration.ofSeconds(10));   //Decalration of class
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//input[@placeholder='Username']"));
+    }
+}
